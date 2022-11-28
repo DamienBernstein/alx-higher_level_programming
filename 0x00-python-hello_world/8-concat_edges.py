bf5e2bce2384e57c3db0222ae5eaa5efa4a9f628
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-11-28 14:16:58 +0200

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[30:-63])
+str += with python + str
+print(str[39:-63])
