c5fd1421e64491dfca050c3fd8c31e57eac2bd32
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-12-02 12:07:23 +0200

    Create 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+  for arr in range(len(my_list)):
+    if (idx < 0):
+      return (none)
+    if (idx > len(my_list)):
+      return (none)
+    elif (idx == array):
+      return (my_list[arr])
+  
