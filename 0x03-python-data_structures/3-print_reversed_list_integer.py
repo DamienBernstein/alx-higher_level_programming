9a0d9018acab50955ba2674a924fb8dbd25c4e9e
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-12-02 12:26:38 +0200

    Create 3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+  if (my_list is not none):
+    my_list.reverse()
+  for arr in range (len(my_list)):
+    print("{:d}".format(my_list[arr]))
+    
