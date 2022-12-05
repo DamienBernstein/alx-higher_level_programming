c3568b1933f122c47de1cfd44aafe73ec42b6355
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-12-05 07:59:22 +0200

    Create 13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include lists.h
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_palindrome - checks if a singly linked
+ * lists is a palindrome
+ * @head: head of stack
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+  
