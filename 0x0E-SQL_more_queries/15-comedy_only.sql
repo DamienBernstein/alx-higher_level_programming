e43ca5e0eb18ded41077522a862b01897b767c9e
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-15 14:25:15 +0200

    Create 15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tvs.title
+FROM tv_shows tvs
+INNER JOIN tv_show_genres it
+ON tvs.id = it.show_id
+INNER JOIN tv_genres tvg
+ON tvg.id = it.genre_id
+WHERE tvg.name = 'Comedy'
+ORDER BY tvs.title ASC;
