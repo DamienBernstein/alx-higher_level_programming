01e895f1c525c9ed026068206d794d6e1dc19ac6
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-15 13:46:38 +0200

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table cities already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
-Use  htbn_0d_usa;
+USE htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL UNIQUE AUTO_INCREMENTED, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
