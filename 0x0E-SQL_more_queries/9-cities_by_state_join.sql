2218a48ff81591f491dd07b276e0b7f377251d65
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-02-15 13:57:28 +0200

    Create 9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
+SELECT c.id c.name s.name
+FROM cities c
+INNER JOIN states s
+ON c.states_id = s.id
+ORDER BY c.id ASC;
