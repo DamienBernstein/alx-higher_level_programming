269d2ee5ff59df87a67d10cb2a5949bf3572905c
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-17 10:51:20 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 
-"""Lists all states from the database hbtn_0e_0_usa.
+"""
+Lists all states from the database hbtn_0e_0_usa.
 
 Usage:
     ./0-select_states.py <mysql username> <mysql password> <database name>
 """
 
-import sys
 import MySQLdb
-
-def main():
-    """Connects to the database and retrieves all states from the states table."""
-    try:
-        db = MySQLdb.connect(
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306)
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states")
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-    except MySQLdb.Error as e:
-        print("Error connecting to the database:", e)
-        sys.exit(1)
-    finally:
-        db.close()
+import sys
 
 if __name__ == "__main__":
-    main()
+    # Connect to the MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+    # Create a cursor object to execute queries
+    cursor = db.cursor()
+
+    # Execute the query to retrieve all states from the database and sort by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the results and print them out
+    for state in cursor.fetchall():
+        print(state)
+
+    # Close the database connection
+    db.close()
