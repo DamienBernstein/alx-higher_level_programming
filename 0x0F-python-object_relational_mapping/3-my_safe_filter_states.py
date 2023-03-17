ae8c59995a97bd288ac9dab441e453811dbc30af
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-17 11:08:26 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,8 +7,8 @@ displays all values in the states table of
 import sys
 import MySQLdb
 
-if __name__ == "__main__"
-db = MySQLdb.connector(user=sys.agrv[1], passwd=sys.argv[2], db=sys.argv[3])
-c = db.cursor()
-c.execute("""SELECT * FROM the states""")
-[print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states""")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
