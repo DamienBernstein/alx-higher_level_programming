9049dfcab8eec7885cf137d33bf23ba025887a62
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-17 10:46:12 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,29 @@
-#!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa
- Usage: ./0-select_states.py <mysql username>
-                            <mysql password>
-                            <database name >
+#!/usr/bin/env python3
+
+"""Lists all states from the database hbtn_0e_0_usa.
+
+Usage:
+    ./0-select_states.py <mysql username> <mysql password> <database name>
 """
 
 import sys
-import mySQLdb
+import MySQLdb
+
+def main():
+    """Connects to the database and retrieves all states from the states table."""
+    try:
+        db = MySQLdb.connect(
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+        sys.exit(1)
+    finally:
+        db.close()
 
 if __name__ == "__main__":
-  db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db.sys.argv[3])
-  c = db.cursor()
-  c.execute("SELECT * FROM 'states'")
-  [print(state) for state in c.fetchall()]
+    main()
